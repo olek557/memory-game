c77111db8f45ab370069111632f7fe2b907c9998
Oleksiy Derkach <oleksiy.derkach@perfectial.com>
2019-02-14 23:47:46 +0200

    EsLint

--- script.js
+++ script.js
@@ -1,96 +1,99 @@
-// BOARD CLASS
+/* eslint-env browser */
+const Card = function(value) {
+  this.value = value;
+};
+
 const Board = function(squareSize) {
   this.sequence = [];
   this.squareSize = squareSize;
   this.size = squareSize * squareSize;
-  this.selectedCard = '';
-}
+  this.selectedCard = "";
+};
 
 Board.prototype.startGame = function() {
+  console.log("here");
   this.generateHTMLBoard();
   this.generateDataBoard();
   this.randomizeDataBoard();
   this.addEventListenerToCards();
-}
+};
 
 Board.prototype.generateHTMLBoard = function() {
-  let board = document.createElement('div');
-  board.classList.add('wrapper');
-  board.id = 'board';
-  for (let i = 0; i < this.size; i++) {
-    let cardWrapper = document.createElement('div'),
-        card = document.createElement('div'),
-        card__front = document.createElement('div'),
-        card__back = document.createElement('div');
-    card__front.classList.add('card__front');
-    card__back.classList.add('card__back');
-    card.classList.add('card');
-    card.setAttribute('data-cell', i);
-    card.appendChild(card__back);
-    card.appendChild(card__front);
-    cardWrapper.classList.add('card__wrapper');
+  const board = document.createElement("div");
+  board.classList.add("wrapper");
+  board.id = "board";
+  for (let i = 0; i < this.size; i += 1) {
+    const cardWrapper = document.createElement("div");
+    const card = document.createElement("div");
+    const cardFront = document.createElement("div");
+    const cardBack = document.createElement("div");
+    cardFront.classList.add("card__front");
+    cardBack.classList.add("card__back");
+    card.classList.add("card");
+    card.setAttribute("data-cell", i);
+    card.appendChild(cardBack);
+    card.appendChild(cardFront);
+    cardWrapper.classList.add("card__wrapper");
     cardWrapper.appendChild(card);
     board.appendChild(cardWrapper);
   }
   document.body.appendChild(board);
-  this.boardElement = document.getElementById('board');
-}
+  this.boardElement = document.getElementById("board");
+};
 
 Board.prototype.generateDataBoard = function() {
-  for (let i = 0; i < this.size / 2; i++) {
+  for (let i = 0; i < this.size / 2; i += 1) {
     this.sequence.push(new Card(i), new Card(i));
   }
-}
+};
 
 Board.prototype.randomizeDataBoard = function() {
-  this.sequence.sort(() => { return 0.5 - Math.random() });
-}
+  this.sequence.sort(() => {
+    return 0.5 - Math.random();
+  });
+};
 
 Board.prototype.addEventListenerToCards = function() {
-  this.boardElement.addEventListener('click', event => {
-    if(event.target.matches('.card__front')) {
-      let cardElement = event.target.parentNode,
-          index = cardElement.getAttribute('data-cell');
-      cardElement.parentNode.classList.add('open');
-      cardElement.querySelector('.card__back').innerHTML = this.sequence[index].value;
-      if(this.selectedCard) {
-        if(this.sequence[index].value == this.selectedCard.value) {
+  this.boardElement.addEventListener("click", event => {
+    if (event.target.matches(".card__front")) {
+      const cardElement = event.target.parentNode;
+      const index = cardElement.getAttribute("data-cell");
+      cardElement.parentNode.classList.add("open");
+      cardElement.querySelector(".card__back").innerHTML = this.sequence[
+        index
+      ].value;
+      if (this.selectedCard) {
+        if (this.sequence[index].value === this.selectedCard.value) {
           setTimeout(() => {
             this.hideCards();
             this.closeAllCards();
           }, 600);
-        }
-        else {
+        } else {
           setTimeout(() => {
             this.closeAllCards();
           }, 600);
         }
-      }
-      else {
-        // this.closeAllCards();
+      } else {
         this.selectedCard = this.sequence[index];
       }
     }
   });
-}
+};
 
 Board.prototype.closeAllCards = function() {
-  this.selectedCard = '';
-  Array.from(document.getElementsByClassName('card__wrapper')).forEach(i => {
-    i.classList.remove('open');
+  this.selectedCard = "";
+  Array.from(document.getElementsByClassName("card__wrapper")).forEach(i => {
+    i.classList.remove("open");
   });
-}
+};
 
 Board.prototype.hideCards = function() {
-  Array.from(document.getElementsByClassName('card__wrapper open')).forEach(i => {
-    i.classList.add('hide');
-  });
-}
-
-// CARD CLASS
-let Card = function(value) {
-  this.value = value;
-}
+  Array.from(document.getElementsByClassName("card__wrapper open")).forEach(
+    i => {
+      i.classList.add("hide");
+    }
+  );
+};
 
 // INIT
 const board = new Board(4);
